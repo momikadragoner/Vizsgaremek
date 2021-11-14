df3b727ab34b695b54d2b86b7ab76cf042d31e85
BuildTools <unconfigured@null.spigotmc.org>
2021-11-14 19:47:55 +0100

    dinamikus termékek

--- agora-angular/src/app/front-page/front-page.component.ts
+++ agora-angular/src/app/front-page/front-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-front-page',
@@ -11,6 +12,15 @@ export class FrontPageComponent implements OnInit {
 
   faShoppingCart = faShoppingCart;
   faSearch = faSearch;
+  faChevronDown = faChevronDown;
+
+  public products = [
+    {productName: "Gyöngy nyakék", sellerName: "Kiss Márta", price: "12 599", imgUrl: "assets/item2.jpg" },
+    {productName: "Arany lánc", sellerName: "Nagy Erzsébet", price: "18 999", imgUrl: "assets/item1.jpg" },
+    {productName: "Ásvány medál ékszer", sellerName: "Széles Lajos", price: "11 999", imgUrl: "assets/item3.jfif" },
+    {productName: "Arany és kristály nyaklánc", sellerName: "Közepes Borbála", price: "19 599", imgUrl: "assets/item4.jpg" },    {productName: "Arany lánc", sellerName: "Nagy Erzsébet", price: "18 999", imgUrl: "assets/item1.jpg" },
+    {productName: "Ásvány medál ékszer", sellerName: "Széles Lajos", price: "11 999", imgUrl: "assets/item3.jfif" }
+  ];
 
   constructor() {}
 
