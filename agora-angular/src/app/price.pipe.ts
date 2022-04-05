6c4de6760592135fee38609a5f213d977f9f6c8e
momikadragoner <you@example.com>
2022-04-05 10:00:30 +0200

    product-card docs

--- agora-angular/src/app/price.pipe.ts
+++ agora-angular/src/app/price.pipe.ts
@@ -3,8 +3,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'price'})
 export class PricePipe implements PipeTransform {
   transform(value: number) {
-    let price:string = value.toString();
-    let length:number = price.length;
+    let price:string = Math.round(value).toString();
+    // let length:number = price.length;
     price = price.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$& ');
     return price + " Ft";
   }
