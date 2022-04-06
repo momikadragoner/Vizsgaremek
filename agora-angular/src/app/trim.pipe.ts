eb6215eebd1e984c1710c5b6f98210b98d98b59a
momikadragoner <you@example.com>
2022-04-06 11:52:12 +0200

    útválasztás

--- agora-angular/src/app/trim.pipe.ts
+++ agora-angular/src/app/trim.pipe.ts
@@ -2,7 +2,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'trim'})
 export class TrimPipe implements PipeTransform {
   transform(value: string, leng = 80): string {
-    let trimmed : string //= value.length < length ? value.substring(0, lenght - 3) + "..." : value + "---";
     if (value.length >= leng)
     {
       value = value.substring(0, leng)
