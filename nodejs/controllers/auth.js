edf54851ed925fcf61484843fdf85aa390adb157
bedialexandra <bedialexandra2003@gmail.com>
2022-04-24 14:01:31 +0200

    ppt + regisztráció tárolt eljárások

--- nodejs/controllers/auth.js
+++ nodejs/controllers/auth.js
@@ -18,7 +18,7 @@ exports.userSignup=(req,res,next)=>{
         const checkEmail =`select email from member where email=?`;
         conn.query(checkEmail,[email],(err,result, fields)=>{
             if(!result.length){
-                const sql =`insert into member (last_name, first_name, email, password, profile_picture_link, header_picture_link, registered_at) values(?,?,?,?,?,?,?)`
+                const sql =`CALL signUp(?,?,?,?,?,?,?)`
                 conn.query(sql,[lastName, firstName, email, hashed_password, "assets/def-pfp1.png", "assets/default_assets/def-bg3.png", new Date()],(err,result, fields)=>{
                     if(err){
                         res.send({status:0, data:err});
@@ -40,15 +40,23 @@ exports.userSignupAsVendor=(req,res,next)=>{
         let{lastName, firstName, email, password}=req.body;
         const hashed_password=md5(password.toString())
         const checkEmail =`select email from member where email=?`;
+        
         conn.query(checkEmail,[email],(err,result, fields)=>{
             if(!result.length){
-                const sql =`insert into member (last_name, first_name, email, password, profile_picture_link, header_picture_link, registered_at, is_vendor) values(?,?,?,?,?,?,?,?)`
-                conn.query(sql,[lastName, firstName, email, hashed_password, "assets/def-pfp1.png", "assets/default_assets/def-bg3.png", new Date(),1],(err,result, fields)=>{
+                conn.query(`CALL insertVendor(?,?,?,?,?,?,?,?)`,[lastName, firstName, email, hashed_password, "assets/def-pfp1.png", "assets/default_assets/def-bg3.png", new Date(),1],(err,result, fields)=>{
                     if(err){
                         res.send({status:0, data:err});
-                    } else{
-                        let token = jwt.sign({data:result},'secret')
-                        res.send({status:1,data:result,token:token});
+                    } else{      
+                        conn.query(`insert into vendor_detail(member_id) select member_id from member where email=?`,[email],(err,result,fields)=>{
+                            if(err){
+                                    console.log(err);
+                                    res.send({status:0, data:err});
+                            }
+                            else{
+                                let token = jwt.sign({data:result},'secret')
+                                res.send({status:1,data:result,token:token});
+                            }
+                        })
                     }
                 })
             }
